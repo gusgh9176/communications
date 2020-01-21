bc6f0414afa522cc5605a92d3bda042fc4f61ae5
gusgh9176 <gusgh9176@naver.com>
2020-01-21 14:48:44 +0900

    추가 SecurityConfig, Role

---
+++ src/main/java/com/example/communications/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.example.communications.config;
+
+
+import com.example.communications.service.UsersService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private UsersService usersService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // 페이지 권한 설정
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/materials/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
+                .and() // 로그인 설정
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/user/login/result")
+                .permitAll()
+                .and() // 로그아웃 설정
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/user/logout/result")
+                .invalidateHttpSession(true)
+                .and()
+                // 403 예외처리 핸들링
+                .exceptionHandling().accessDeniedPage("/user/denied")
+        // 프레임 옵션, csrf 토큰 비활성화 (h2 db 테스트용)
+//                .and()
+//                .headers().frameOptions().disable()
+//                .and()
+//                .csrf().ignoringAntMatchers("/**")
+        ;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersService).passwordEncoder(passwordEncoder());
+    }
+}
