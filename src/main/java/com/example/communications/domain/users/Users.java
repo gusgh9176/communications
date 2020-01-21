8b4da2fa1610eb853fcdc5b28a68493582987dd3
gusgh9176 <gusgh9176@naver.com>
2020-01-21 15:44:48 +0900

    추가 Users 변수, 메소드
    +
    password
    prePersists()

--- src/main/java/com/example/communications/domain/users/Users.java
+++ src/main/java/com/example/communications/domain/users/Users.java
@@ -21,6 +21,9 @@ public class Users extends BaseTimeEntity {
     @Column(length = 500, nullable = false)
     private String name;
 
+    @Column(length = 100, nullable = false)
+    private String password;
+
     @Column(nullable = false)
     private Integer victory;
 
@@ -30,9 +33,19 @@ public class Users extends BaseTimeEntity {
     @Column(nullable = false)
     private String results;
 
+    // null 이면 0 값 or 빈 스트링 값 넣어서 insert
+    @PrePersist
+    public void prePersist() {
+        this.victory = ((this.victory == null) ? 0 : this.victory);
+        this.rank = ((this.rank == null) ? 0 : this.rank);
+        this.results = ((this.results == null) ? "" : this.results);
+    }
+
     @Builder
-    public Users(String name, Integer victory, Integer rank, String results) {
+    public Users(Long id, String name, String password, Integer victory, Integer rank, String results) {
+        this.id = id;
         this.name = name;
+        this.password = password;
         this.victory = victory;
         this.rank = rank;
         this.results = results;
