c5d07ec0132b33ff63c449b6ed901a63cc013929
gusgh9176 <gusgh9176@naver.com>
2020-01-22 17:10:58 +0900

    변경 @EnableAuditing
    @EnableJpaAuditing
    to
    @EnableMongoAuditing

--- src/main/java/com/example/communications/CommunicationsApplication.java
+++ src/main/java/com/example/communications/CommunicationsApplication.java
@@ -2,10 +2,10 @@ package com.example.communications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
-@EnableJpaAuditing
+@EnableMongoAuditing
 @SpringBootApplication
 public class CommunicationsApplication {
 
