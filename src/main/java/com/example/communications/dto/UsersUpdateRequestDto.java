65fe8724d66a8dd86208195415cbc7551b808395
gusgh9176 <gusgh9176@naver.com>
2020-01-24 13:35:22 +0900

    변경 UpdateRequestDto 생성자
    +
    id

--- src/main/java/com/example/communications/dto/UsersUpdateRequestDto.java
+++ src/main/java/com/example/communications/dto/UsersUpdateRequestDto.java
@@ -1,17 +1,23 @@
 package com.example.communications.dto;
 
 import com.example.communications.domain.users.Users;
-import lombok.Builder;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class UsersUpdateRequestDto {
 
+    private String id;
     private String name;
     private Integer victory;
     private Integer rank;
     private String results;
 
     @Builder
-    public UsersUpdateRequestDto(String name, Integer victory, Integer rank, String results) {
+    public UsersUpdateRequestDto(String id, String name, Integer victory, Integer rank, String results) {
+        this.id = id;
         this.name = name;
         this.victory = victory;
         this.rank = rank;
@@ -20,6 +26,7 @@ public class UsersUpdateRequestDto {
 
     public Users toEntity(){
         return Users.builder()
+                .id(id)
                 .name(name)
                 .victory(victory)
                 .rank(rank)
