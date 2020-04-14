ad641fe1462282706b84acbf03ea6e8c8efff56d
SeRDic0705 <SeRDic0705@github.com>
2020-04-15 02:16:05 +0900

    bin파일 변경
    repositary를 다시 fork하면서 변경점 발생

--- bin/main/static/js/app/chatClient.js
+++ bin/main/static/js/app/chatClient.js
@@ -1,9 +1,17 @@
+// input 태그에서 엔터키 누를시 전송 버튼 클릭 이벤트로 변경
+$('input[type="text"]').keydown(function() {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		$('#sendBtn').click();
+	};
+});
+
 $(function() {
 		var messageBox = $('#messageBox');
 		var messageInput = $('#messageInput');
 		var sendBtn = $('#sendBtn');
-		var name = "user";
-		
+		var name = $('#username').text();
+
 		var socket = new SockJS('/websocket');	
 		var client = Stomp.over(socket);		//SockJS와 client 연결
 		
@@ -13,13 +21,14 @@ $(function() {
 			//메인 채팅 구독
 			client.subscribe('/topic/chat', function (chat) {
 		    	var content = JSON.parse(chat.body);
-		    	messageBox.append("<li>" + content.name + " : " + content.message + "</li>")
+		    	messageBox.append("<li>" + content.name + " : " + content.message + "</li>");
+				$("#messageDiv").scrollTop($("#messageDiv")[0].scrollHeight); // 채팅 전송시 스크롤 자동으로 내려줌
 			});
 			//귓속말 구독
 			client.subscribe('/queue/' + name, function (chat) {
 				var content = JSON.parse(chat.body);
 				messageBox.append("<li class='whisper'>" + content.name + ">>" + content.message + "</li>")
-			})
+			});
 			
 			client.send("/app/join", {}, JSON.stringify({name: name, message: ""}));
 		});
