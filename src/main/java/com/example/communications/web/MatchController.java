ff87e7911f0a59f1c3e75a7010480399e76c98f5
SeRDic0705 <SeRDic0705@github.com>
2020-04-24 02:37:53 +0900

    matchSet 추가
    long polling 방식으로 통신 재시도 시, matchQueue에 중복으로 등록되는 문제 방지

--- src/main/java/com/example/communications/web/MatchController.java
+++ src/main/java/com/example/communications/web/MatchController.java
@@ -1,8 +1,10 @@
 package com.example.communications.web;
 
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import com.example.communications.domain.game.GameManager;
 public class MatchController {
 	protected final Map<Integer, GameManager> gameManagers = new ConcurrentHashMap<Integer, GameManager>();
 	private Queue<String> matchQueue = new LinkedList<String>();
+	private Set<String> matchSet = new LinkedHashSet<String>();			//matchQueue에 중복 등록되는지 체크
 	private boolean isMatched = true;
 	private int Hash;
 	
@@ -30,7 +33,10 @@ public class MatchController {
 	
 	@GetMapping("/game/match")
 	public int getGameHash(@RequestParam String id) {
-		this.matchQueue.add(id);
+		if(matchSet.add(id)) {		//set에 중복값 존재하지 않을 시 true반환
+			this.matchQueue.add(id);
+		}
+		
 		
 		while(true) {
 			if(matchQueue.peek() == id) {	//queue의 top에 있는지 검사
@@ -38,11 +44,13 @@ public class MatchController {
 					this.setNewHash();
 					this.isMatched = false;
 					this.matchQueue.remove();
+					this.matchSet.remove(id);
 					return this.Hash;
 				}
 				else {
 					this.isMatched = true;
 					this.matchQueue.remove();
+					this.matchSet.remove(id);
 					this.gameManagers.put(this.Hash, new GameManager());
 					return this.Hash;
 				}
